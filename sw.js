faf6e783894d92b148ea4a1a4778dfd6222e11db
dogc7717-boop <dogc7717@gmail.com>
2026-02-18 15:53:33 +0200

    Refactor service worker for caching assets

--- sw.js
+++ sw.js
@@ -1,28 +1,8 @@
-const cacheName = 'iskar-v2'; // قمت بتغيير النسخة للتحديث
-const assets = [
-  './',
-  './index.html',
-  './manifest.json',
-  './Egypt.mp3',
-  './Egypt_1.mp3',
-  './Egypt_2.mp3',
-  './Egypt_3.mp3',
-  'https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&display=swap',
-  'https://assets.mixkit.co/active_storage/sfx/3005/3005-preview.mp3'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
+ const CACHE_NAME = 'azkar-v2';
+const ASSETS = ['./', './index.html', './style.css', './script.js', './manifest.json'];
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
