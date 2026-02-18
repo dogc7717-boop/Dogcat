31f8a3fa999629349f9bfd375a4dd5314220be4c
dogc7717-boop <dogc7717@gmail.com>
2026-02-18 13:48:31 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,28 @@
-const cacheName = 'iskar-v1';
+const cacheName = 'iskar-v2'; // قمت بتغيير النسخة للتحديث
 const assets = [
   './',
   './index.html',
-  './script.js',
   './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&display=swap'
+  './Egypt.mp3',
+  './Egypt_1.mp3',
+  './Egypt_2.mp3',
+  './Egypt_3.mp3',
+  'https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&display=swap',
+  'https://assets.mixkit.co/active_storage/sfx/3005/3005-preview.mp3'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
